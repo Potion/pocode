a7c6dcf3027000a297211401cebd39b39beca877
josh <josh@potiondesign.com>
2011-03-16 17:39:37 -0400

    remove everything, start afresh

--- src/poModifier.cpp
+++
@@ -1,6 +0,0 @@
-/*
- *  Copyright 2008 Potion Design LLC. All rights reserved.
- */
-
-#include "poModifier.h"
-
--- xcode/main_test.cpp
+++
@@ -1,6 +0,0 @@
-/*
- *  Copyright 2011 Potion Design. All rights reserved.
- */
-
-#define BOOST_TEST_MODULE poFrameworkTestSuite
-#include <boost/test/unit_test.hpp>
