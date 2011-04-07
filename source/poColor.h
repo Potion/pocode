a1fc619b7698c353d725f0ca25951e107de02662
Jared Schiffman <jared@js-mbp.local>
2011-04-07 18:31:54 -0400

    changed poColor

--- source/poColor.h
+++ source/poColor.h
@@ -4,13 +4,6 @@
 
 #pragma once
 
-struct HSVColor {
-	float hue, saturation, value;
-	
-	HSVColor() : hue(0.f), saturation(0.f), value(0.f) {}
-	HSVColor(float h, float s, float v) : hue(h), saturation(s), value(v) {}
-};
-
 class poColor
 {
 public:
@@ -18,14 +11,17 @@ public:
 	
 	poColor();
 	poColor(float r, float g, float b, float a=1.f);
+    poColor(const poHSVColor &hsv);
 	
 	poColor &set(float r, float g, float b, float a=1.f);
-	poColor &setHSV(float h, float s, float v);
-	poColor &setHSV(HSVColor hsv);
 };
 
-// will pre-multiply the alpha
-HSVColor rgba2hsv(poColor rgba);
-// will come back with alpha = 1.f
-poColor hsv2rgba(HSVColor hsv);
 
+class poHSVColor {
+public:
+	float hue, saturation, value;
+	
+	HSVColor() : hue(0.f), saturation(0.f), value(0.f) {}
+	HSVColor(float h, float s, float v) : hue(h), saturation(s), value(v) {}
+    HSVColor &set(float h, float s, float v);
+};
