2b9807abdf5bdc96a26bd9f75435e2a007cf3b60
josh <josh@potiondesign.com>
2011-06-01 13:24:19 -0400

    added image mask (black/white greyscale image) as poObjectModifier

--- source/poMaskModifier.cpp
+++ source/poMaskModifier.cpp
@@ -7,3 +7,32 @@
 //
 
 #include "poMaskModifier.h"
+#include "poSimpleDrawing.h"
+
+poImageMask::poImageMask(poTexture *tex)
+:	texture(tex->copy())
+{}
+
+poImageMask::poImageMask(const std::string &str) {
+	poImage *img = new poImage(str, IMAGE_8);
+	texture = new poTexture(GL_ALPHA, img->width(), img->height(), img->storageSize(), img->pixels());
+	delete img;
+}
+
+poImageMask::~poImageMask() {
+	delete texture;
+}
+
+void poImageMask::setUp( poObject* obj ) {
+	glPushAttrib(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_BLEND);
+	
+	glBlendFuncSeparate(GL_ZERO, GL_DST_COLOR, GL_SRC_COLOR, GL_ZERO);
+	drawRect(texture);
+	
+	glBlendFunc(GL_DST_ALPHA, GL_ONE_MINUS_DST_ALPHA);
+}
+
+void poImageMask::setDown( poObject* obj ) {
+	glPopAttrib();
+}
--- source/poMaskModifier.h
+++ source/poMaskModifier.h
@@ -5,3 +5,23 @@
 //  Created by Joshua Fisher on 6/1/11.
 //  Copyright 2011 Potion Design. All rights reserved.
 //
+
+#pragma once
+
+#include "poTexture.h"
+#include "poObjectModifier.h"
+
+class poImageMask 
+:	public poObjectModifier
+{
+public:
+	poImageMask(poTexture *tex);
+	poImageMask(const std::string &str);
+	virtual ~poImageMask();
+	
+	virtual void setUp( poObject* obj );
+	virtual void setDown( poObject* obj );
+	
+private:
+	poTexture *texture;
+};
