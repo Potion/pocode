84e10dee3f770dca84206de1078bf58ac95314b9
Jonathan Bobrow <jonathan@potiondesign.com>
2011-08-09 03:43:05 -0400

    new scribble sample in progress

---
+++ samples/_Scribble/Scribble/Dot.cpp
@@ -0,0 +1,9 @@
+//
+//  Dot.cpp
+//  Scribble
+//
+//  Created by Jonathan Bobrow on 8/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ samples/_Scribble/platform/AppDelegate.h
@@ -0,0 +1,24 @@
+#import <Cocoa/Cocoa.h>
+
+#include "poApplication.h"
+
+class poWindow;
+
+@interface AppDelegate : NSObject <NSApplicationDelegate> 
+{
+	NSOpenGLContext *shared_context;
+	poWindow *currentWindow;
+}
+
+@property (nonatomic,assign) poWindow *currentWindow;
+
+-(void)quit;
+-(poWindow*)createWindow:(uint)root_obj_id 
+					type:(poWindowType)type 
+				   frame:(NSRect)frame
+				   title:(const char*)str;
+
+-(void)closeWindow:(poWindow*)win;
+-(void)fullscreenWindow:(poWindow*)win value:(BOOL)b;
+
+@end
