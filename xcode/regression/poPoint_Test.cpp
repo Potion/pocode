ede46c584f5bb8361e26924d884aff12602073c4
Vtron <steve@vargatron.com>
2011-09-19 16:04:12 -0400

    Revert "Revert "Added poSqlite3 to poFramework4""
    This reverts commit 0e3650c353be0dc9b72ad89a579af70b104cdb39.

--- xcode/regression/poPoint_Test.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/test/unit_test.hpp>
-
-#include "poPoint.h"
-
-BOOST_AUTO_TEST_CASE( point_test_1 ) {
-	poPoint p1(100,100);
-    BOOST_CHECK_CLOSE(p1.x, 100.0, 0.0000001);
-    BOOST_CHECK_CLOSE(p1.y, 100.0, 0.0000001);
-}
