e2a092f6ffb985e8040194b45f3e48dd5a95fb56
josh <josh@potiondesign.com>
2011-10-20 16:55:49 -0400

    fuller test of ios rendering horses

--- templates/iOS_tmp/potionCode/TestObj.cpp
+++ templates/iOS_tmp/potionCode/TestObj.cpp
@@ -15,27 +15,50 @@
 #include "poGeometryMask.h"
 
 poObject *createObjectForID(uint id) {
+	setCurrentPath("resource");
 	return new TestObj();
 }
 
 TestObj::TestObj() {
 	addModifier(new poCamera2D(poColor::white));
 	
+	holder = addChild(new poObject());
+	holder = holder->addChild(new poObject());
+	holder = holder->addChild(new poObject());
+	holder = holder->addChild(new poObject());
+	
 	for(int i=0; i<100; i++) {
-		poTextBox *text = addChild(new poTextBox(200));
+		poShape2D *shp = holder->addChild(new poRectShape(100,100));
+		shp->alpha = poMapf(0, 100, i, .2, .7);
+		shp->position = randomPointInRect(getWindowFrame());
+		shp->placeTexture(poTexture(getImage("apple.jpg")), PO_TEX_FIT_INSIDE);
+		shp->fillColor = poColor::orange;
+		shp->generateStroke(3);
+	}
+	
+	for(int i=0; i<100; i++) {
+		poTextBox *text = holder->addChild(new poTextBox(100));
 		text->font(getFont("Maharam-Regular.otf",""));
 		text->textSize(poRand(12,20));
-		text->textColor = poColor::red;
+		text->textColor = poColor::blue;
 		text->text(std::string("hello:") + boost::lexical_cast<std::string>(text->textSize()));
+		text->textAlignment(PO_ALIGN_CENTER);
 		text->leading(.7f);
 		text->position = randomPointInRect(getWindowFrame());
 		text->layout();
 	}
+	
+	poShape2D *mask = new poOvalShape(500,500,20);
+	mask->position = getWindowCenter();
+	addModifier(new poGeometryMask(mask));
 }
 
 void TestObj::update() {
-	for(int i=0; i<100; i++) {
-		poTextBox *text = getChildAs<poTextBox>(i);
+	holder->alpha = poMapf(-1,1, sin(getTime()), .5,1);
+	
+	for(int i=0; i<200; i++) {
+		poObject *obj = holder->getChild(i);
+		obj->rotation += (i<100 ? -1.f : 1.f);
 	}
 }
 
--- templates/iOS_tmp/potionCode/TestObj.h
+++ templates/iOS_tmp/potionCode/TestObj.h
@@ -17,4 +17,6 @@ public:
 	void update();
     void eventHandler(poEvent *event);
 	
+	poObject *holder;
+	
 };
