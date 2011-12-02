dbbf44997023eb8b867207919033cdce9641c5f8
Steve Varga <steve@vargatron.com>
2011-12-02 15:07:41 -0500

    Syntax stuff with removeTexture()

--- source/poShape2D.cpp
+++ source/poShape2D.cpp
@@ -170,8 +170,10 @@ poTexture* poShape2D::getTexture() {
 }
 
 void poShape2D::removeTexture(bool andDelete) {
-    if(andDelete && texture) delete texture;
-    texture = NULL;
+    if(andDelete && texture) {
+        delete texture;
+        texture = NULL;
+    }
 }
 
 poShape2D& poShape2D::transformTexture(poPoint pt, poPoint scale, float rotate) {
