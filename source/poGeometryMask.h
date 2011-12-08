80aa3da86716ea37bf222b37b970d56ce160b056
Ilan <ilan@Ilans-Mac-Pro.local>
2011-12-08 11:42:05 -0500

    added inverse flag to poGeometryMask

--- source/poGeometryMask.h
+++ source/poGeometryMask.h
@@ -23,7 +23,7 @@
 
 class poGeometryMask : public poObjectModifier {
 public:
-	poGeometryMask(poShape2D *shape, bool clearsStencil=false);
+	poGeometryMask(poShape2D *shape, bool clearsStencil=false, bool inverse=false);
 	virtual			~poGeometryMask();
 	
 	virtual			poObjectModifier *copy();
@@ -39,6 +39,7 @@ protected:
 private:
 	poShape2D*		shape;
 	bool			clearsStencil;
+    bool            inverse;
 };
 
 /*
