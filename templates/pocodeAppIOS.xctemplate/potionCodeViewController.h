c699f3d687199e6d98a7be06e5704ad6907f2e77
Jared Schiffman <jared@potiondesign.com>
2012-01-10 19:47:39 -0500

    created iOS template and updated OSX template

---
+++ templates/pocodeAppIOS.xctemplate/potionCodeViewController.h
@@ -0,0 +1,36 @@
+//
+//  potionCodeViewController.h
+//  potionCode
+//
+//  Created by Joshua Fisher on 9/21/11.
+//  Copyright 2011 Potion Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+
+#include "poHelpers.h"
+#include "poWindow.h"
+#import "EAGLView.h"
+
+@interface potionCodeViewController : UIViewController<UIAlertViewDelegate> {
+    EAGLContext *context;
+     
+    BOOL animating;
+    NSInteger animationFrameInterval;
+    CADisplayLink *displayLink;
+} 
+
+@property (nonatomic, assign) IBOutlet EAGLView *eagl;
+
+@property (nonatomic, readonly) poWindow *appWindow;
+@property (readonly, nonatomic, getter=isAnimating) BOOL animating;
+@property (nonatomic) NSInteger animationFrameInterval;
+
+- (void)startAnimation;
+- (void)stopAnimation;
+
+@end
---
+++ templates/pocodeAppOSX.xctemplate/___PROJECTNAMEASIDENTIFIER___App.cpp
@@ -0,0 +1,37 @@
+/*	Created by ___FULLUSERNAME___ on ___DATE___.
+ *	Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+ */
+
+#include "___PROJECTNAMEASIDENTIFIER___App.h"
+#include "poApplication.h"
+#include "poCamera.h"
+
+
+// APP CONSTRUCTOR. Create all objects here.
+___PROJECTNAMEASIDENTIFIER___App::___PROJECTNAMEASIDENTIFIER___App() {
+	addModifier(new poCamera2D(poColor::black));
+}
+
+// APP DESTRUCTOR. Delete all objects here.
+___PROJECTNAMEASIDENTIFIER___App::~___PROJECTNAMEASIDENTIFIER___App() {
+}
+
+// UPDATE. Called once per frame. Animate objects here.
+void ___PROJECTNAMEASIDENTIFIER___App::update() {
+	
+}
+
+// UPDATE. Called once per frame. Draw objects here.
+void ___PROJECTNAMEASIDENTIFIER___App::draw() {
+	
+}
+
+// EVENT HANDLER. Called when events happen. Respond to events here.
+void ___PROJECTNAMEASIDENTIFIER___App::eventHandler(poEvent *event) {
+	
+}
+
+// MESSAGE HANDLER. Called from within the app. Use for message passing.
+void ___PROJECTNAMEASIDENTIFIER___App::messageHandler(const std::string &msg, const poDictionary& dict) {
+	
+}
