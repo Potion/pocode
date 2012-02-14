3d3a1696b1b7b90ff94261bd8ae262d84a0f43f1
Filippo Vanucci <filippo.vanucci@gmail.com>
2012-02-14 16:29:03 -0500

    added poLayout + UIControl copyrights

--- source/poControlPanel.h
+++ source/poControlPanel.h
@@ -1,3 +1,24 @@
+/*
+ *	Copyright 2012 Potion Design. All rights reserved.
+ *	This file is part of pocode.
+ *
+ *	pocode is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Lesser General Public License as 
+ *	published by the Free Software Foundation, either version 3 of 
+ *	the License, or (at your option) any later version.
+ *
+ *	pocode is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public 
+ *	License along with pocode.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
 //
 //  UIWindow.h
 //  controlUI
@@ -12,8 +33,6 @@
 #include "poTextBox.h"
 #include "poDictionary.h"
 #include <set>
-
-#pragma once
 
 using namespace std;
 
@@ -66,5 +85,4 @@ public:
     poDictionary settings;
 
 };
-
 
