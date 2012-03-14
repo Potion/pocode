7eadf058480c829f5e73c5367eb8e5bf0cc57b88
Filippo Vanucci <filippo.vanucci@gmail.com>
2012-03-14 17:14:11 -0400

    Control panel example commented

--- examples/01_Essentials/03_Application_02_ControlPanel/ControlPanel/ControlPanelApp.cpp
+++ examples/01_Essentials/03_Application_02_ControlPanel/ControlPanel/ControlPanelApp.cpp
@@ -20,25 +20,40 @@ ControlPanelApp::ControlPanelApp() {
     poImageShape* BG = new poImageShape("bg.jpg");
     addChild( BG );
 	
-	control = new poControlPanel( "controlPanel", poColor(0,0,0,0.6) );
-    control->addToggle( "visible", this );
-    control->addSliderF( "scale",0.5,5, this );
-    control->addPointSlider( "loc", poPoint(50,172), poPoint(508,467), this );
-    control->addColorSlider( "color", true, this );
-    control->addKnob( "knob",0, 360, this );
+	control = new poControlPanel( "controlPanel", poColor(0,0,0,0.6) );	// Create a control panel
+																		// Give it a name and a
+																		// background color
 	
-	std::vector<std::string> radio_options;
-	radio_options.push_back("triangle");
-	radio_options.push_back("quadrilateral");
-	radio_options.push_back("pentagon");
-	radio_options.push_back("hexagon");
-	radio_options.push_back("octagon");
-	radio_options.push_back("circle");
-	control->addRadio("shape", radio_options, this);
+	// CONTROLS
+	// Each control in the control panel has a name and a pointer to a listener poObject
+	// So that every time the user touches a button or a slider, a message gets sent
+	// to the listener object
 	
-	control->addInputTextBox( "text",this );
-	control->addSliderI( "size",10,30, this );
+    control->addToggle( "visible", this );								// Add a toggle button called "visible"
+    control->addSliderF( "scale",0.5f, 5.f, this );						// Add a float slider called "scale" with
+																		// values that go from 0.5 to 5
 	
+    control->addPointSlider( "loc", poPoint(50,172), poPoint(508,467), this ); // Add a poPoint slider called "loc"
+																		// and set the min and max poPoint values
+    control->addColorSlider( "color", true, this );						// Add a color slider called "color"
+																		// (it includes R,G,B and alpha sliders)
+    control->addKnob( "knob",0.f, 360.f, this );						// Add a knob called "knob"
+																		// with values that go from 0 to 360
+	
+	// To add a list of radio buttons we need to define first a list of button labels
+	std::vector<std::string> radioOptions;
+	radioOptions.push_back("triangle");
+	radioOptions.push_back("quadrilateral");
+	radioOptions.push_back("pentagon");
+	radioOptions.push_back("hexagon");
+	radioOptions.push_back("octagon");
+	radioOptions.push_back("circle");
+	
+	// Now we can create the a list of radio buttons called "shape", by passing the list of strings that we created
+	control->addRadio("shape", radioOptions, this);
+	
+	// Every option in the radio list corresponds to a different number of vertices for an oval shape
+	// 3 vertices make a triangle, 4 vertices make a quadrilateral, etc.
 	shapeOptions.push_back(3);
 	shapeOptions.push_back(4);
 	shapeOptions.push_back(5);
@@ -46,24 +61,39 @@ ControlPanelApp::ControlPanelApp() {
 	shapeOptions.push_back(8);
 	shapeOptions.push_back(40);
 	
-	int optionID = control->getInt("shape");
+	// Create an oval shape
+	shape = new poOvalShape(40,40, 40);
 	
-	shape = new poOvalShape(40,40, shapeOptions[optionID]);
-	shape->fillColor = control->getColor("color");
-	shape->position = control->getPoint("loc");
+	// SAVED VALUES
+	// The control panel allows you to save the values in an XML file.
+	// Every time the project runs, we are going to set the color, position, scale, etc.
+	// to be the values that were previously saved in the control panel
+	
+	shape->fillColor = control->getColor("color");				// Get a poColor value from the control called "color"
+	shape->position = control->getPoint("loc");					// Get a poPoint value from the control called "loc"
+																// Get a float value from the control called "scale"
 	shape->scale.set(control->getFloat("scale"),control->getFloat("scale"),1);
-	shape->visible = control->getBool("visible");
-	shape->rotation = control->getInt("knob");
+	shape->visible = control->getBool("visible");				// Get a boolean from the control called "visible"
+	shape->rotation = control->getInt("knob");					// Get a float value from the control called "knob"
+	
+	int optionID = control->getInt("shape");			// Get an integer value from the radio buttons called "shape"
+														// The integer is the item ID in the vector list of options
+	shape->reshape(40,40, shapeOptions[optionID]);		// Reshape the oval shape based on the option
+	
+	control->addInputTextBox( "text",this );							// Add an input text box called "text"
+	control->addSliderI( "size",10,30, this );							// Add an integer slider called "size"
+																		// with values from 10 to 30
 	
 	text = new poTextBox(450,295);
-	text->setText(control->getString("text"));
+	text->setText(control->getString("text"));					// Get a string value from the control called "text"
 	text->setFont(poGetFont("Helvetica", "Bold"));
-	text->setTextSize(control->getInt("size"));
+	text->setTextSize(control->getInt("size"));					// Get a integer value from the control called "size"
 	text->textColor = poColor::black;
 	text->setTextAlignment(PO_ALIGN_CENTER_CENTER);
 	text->doLayout();
 	text->position.set(50, 172, 0);
 	
+	// Add all the children in order, so that the control panel is on top of everything;
 	addChild(shape);
 	addChild(text);
 	addChild(control);
@@ -90,6 +120,10 @@ void ControlPanelApp::eventHandler(poEvent *event) {
 
 // MESSAGE HANDLER. Called from within the app. Use for message passing.
 void ControlPanelApp::messageHandler(const std::string &msg, const poDictionary& dict) {
+	
+	// CONTROL PANEL MESSAGES
+	// Every time the user modifies one of the controls in the control panel,
+	// The listener object receives a message with the name of the control UI that was modified
 	
 	if ( msg == "loc" ) {
         shape->position = control->getPoint("loc");
