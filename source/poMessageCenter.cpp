8a2506c884bee9b0fd34d8bab0e9dd941a367ef3
tamarziv <tamarziv@gmail.com>
2013-02-22 16:56:46 -0500

    messageCenter bug fixed

--- source/poMessageCenter.cpp
+++ source/poMessageCenter.cpp
@@ -22,9 +22,10 @@ namespace poMessageCenter {
     //------------------------------------------------------------------
     void update() {
         //Go through queue, broadcasting messages
-        for(std::vector<poMessage*>::iterator mIter = messageQueue.begin(); mIter != messageQueue.end(); ++mIter) {
-            poMessage* m = (*mIter);
-            
+		std::vector<poMessage*>::iterator it = messageQueue.begin();
+		while(it != messageQueue.end()) {
+			poMessage* m = *it;
+			
             //Go through subscribers for this message, checking to see if they need to be alerted
             for (std::vector<poMessageSubscriber* >::iterator sIter = subscribers[m->message].begin(); sIter != subscribers[m->message].end(); ++sIter) {
                 poMessageSubscriber* thisSubscriber = (*sIter);
@@ -33,11 +34,28 @@ namespace poMessageCenter {
                     (*sIter)->subscriber->messageHandler(m->message, m->dict);
                 }
             }
-            
-            delete m; m=NULL;
-        }
-        
-        messageQueue.clear();
+			
+			delete m;
+			it = messageQueue.erase(it);
+		}
+		
+		
+//        for(std::vector<poMessage*>::iterator mIter = messageQueue.begin(); mIter != messageQueue.end(); ++mIter) {
+//            poMessage* m = (*mIter);
+//            
+//            //Go through subscribers for this message, checking to see if they need to be alerted
+//            for (std::vector<poMessageSubscriber* >::iterator sIter = subscribers[m->message].begin(); sIter != subscribers[m->message].end(); ++sIter) {
+//                poMessageSubscriber* thisSubscriber = (*sIter);
+//                
+//                if(thisSubscriber->sender == NULL || thisSubscriber->sender == m->sender) {
+//                    (*sIter)->subscriber->messageHandler(m->message, m->dict);
+//                }
+//            }
+//            
+//            delete m; m=NULL;
+//        }
+//        
+//        messageQueue.clear();
     }
     
     
@@ -88,6 +106,5 @@ namespace poMessageCenter {
             messageQueue.back()->dict       = dict;
         }
     }
-
 }
 
