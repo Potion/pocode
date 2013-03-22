6795503f7b72b0e42f146cd044b6fabddc53cd37
Steve Varga <steve@vargatron.com>
2013-03-22 17:43:33 -0400

    Scaled back number of threads, need to let apps set dynamically, iPad can't handle 10

--- source/poThreadCenter.cpp
+++ source/poThreadCenter.cpp
@@ -29,7 +29,7 @@ namespace po {
         //Start all threads
         void init() {
             //Create threads
-            for(int i=0; i<10; i++) {
+            for(int i=0; i<1; i++) {
                 threads.create_thread(boost::bind(&io_service::run, &service));
             }
         }
